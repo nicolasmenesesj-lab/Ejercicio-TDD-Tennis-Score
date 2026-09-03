eb0d15a5ece3d224695bc07d9c2ba4045cbea891
Nico Meneses <menesesnico20@gmail.com>
2026-09-02 23:09:27 -0400

    feat: Se agrego el html y se edito el presenter para mostrar la interfaz de el juego de tennis

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,29 @@
-import sumar from "./sumador";
+import Tennis from "./tennis";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+let game = new Tennis();
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const player1Button = document.querySelector("#punto-jugador1");
+const player2Button = document.querySelector("#punto-jugador2");
+const resetButton = document.querySelector("#reiniciar");
+const div = document.querySelector("#marcador-div");
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+function mostrarMarcador() {
+  div.innerHTML = "<p>" + game.getScore() + "</p>";
+}
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+player1Button.addEventListener("click", () => {
+  game.player1Scores();
+  mostrarMarcador();
 });
+
+player2Button.addEventListener("click", () => {
+  game.player2Scores();
+  mostrarMarcador();
+});
+
+resetButton.addEventListener("click", () => {
+  game = new Tennis();
+  mostrarMarcador();
+});
+
+mostrarMarcador();
