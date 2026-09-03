50e632c284778378b2820910d3221752fe1d2e7f
Nico Meneses <menesesnico20@gmail.com>
2026-09-02 22:49:48 -0400

    feat: Refactorizacion de la logica de negocios completa pasa todos los tests

--- src/tennis.js
+++ src/tennis.js
@@ -13,42 +13,23 @@ class Tennis {
     }
 
     getScore() {
-        if (this.player1Points === 0 && this.player2Points === 0) {
-            return "Love - Love";
+        const scoreNames = ["Love", "15", "30", "40"];
+        if (this.player1Points >= 3 && this.player2Points >= 3) {
+            if (this.player1Points === this.player2Points) {
+                return "Deuce";
+            } else if (this.player1Points === this.player2Points + 1) {
+                return "Advantage for Player 1";
+            } else if (this.player2Points === this.player1Points + 1) {
+                return "Advantage for Player 2";
+            }
         }
-        if (this.player1Points === 1 && this.player2Points === 0) {
-            return "15 - Love";
-        }
-        if (this.player1Points === 0 && this.player2Points === 1) {
-            return "Love - 15";
-        }
-        if (this.player1Points === 2 && this.player2Points === 0) {
-            return "30 - Love";
-        }
-        if (this.player1Points === 0 && this.player2Points === 2) {
-            return "Love - 30";
-        }
-        if (this.player1Points === 3 && this.player2Points === 0) {
-            return "40 - Love";
-        }
-        if (this.player1Points === 0 && this.player2Points === 3) {
-            return "Love - 40";
-        }
-        if (this.player1Points === 4 && this.player2Points === 0) {
+        if (this.player1Points >= 4 && this.player1Points >= this.player2Points + 2) {
             return "Game for Player 1";
         }
-        if (this.player1Points === 0 && this.player2Points === 4) {
+        if (this.player2Points >= 4 && this.player2Points >= this.player1Points + 2) {
             return "Game for Player 2";
         }
-        if (this.player1Points === 3 && this.player2Points === 3) {
-            return "Deuce";
-        }
-        if (this.player1Points === 4 && this.player2Points === 3) {
-            return "Advantage for Player 1";
-        }
-        if (this.player1Points === 3 && this.player2Points === 4) {
-            return "Advantage for Player 2";
-        }
+        return `${scoreNames[this.player1Points]} - ${scoreNames[this.player2Points]}`;
     }
 }
 export default Tennis;
